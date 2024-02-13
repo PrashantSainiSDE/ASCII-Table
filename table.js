5811de011a66f803e4dd86faadbdbd74fdfecab4
Prince34998 <112163728+Prince34998@users.noreply.github.com>
2024-02-13 12:09:11 +0530

    table generated 1-20

--- table.js
+++ table.js
@@ -10,23 +10,25 @@ const tableArray = [
 ];
 
 tableArray.shift();
-// console.log(tableArray);
 
 // With recursion
 var result = dyArray(20);
-function dyArray(num,arr = []){
-    if(num>0){
+function dyArray(num, arr = []) {
+    if (num > 0) {
         arr.push(num);
-        return dyArray(num-1, arr);
+        return dyArray(num - 1, arr);
     }
-        return arr;
+    return arr;
 }
 result = result.reverse();
-// console.log("With recursion:-",result);
 
 
- console.log(result.map((value)=>value+12));
+const res = result.map((value, index, array) => {
+    return array.filter((a) => a <= 10);
+});
+const last = res.map((val, idx) => val.map((value) => value * (idx + 1)));
 
+console.log(last);
 
 // Create an ASCII table
 // function createASCIITable(row, col, arr) {
@@ -48,36 +50,36 @@ result = result.reverse();
 // Create an ASCII table
 // function createASCIITable(row, col, arr) {
 //   if (row > 0) {
-    //     let rowStr = '';
-    //     for (let i = 0; i < col; i++) {
-        //       rowStr += `${arr[(row - 1) * col + i]}\t`;
-        //     }
-        //     return createASCIITable(row - 1, col, arr) + rowStr + '\n';
-        //   }
-        //   return '';
-        // }
-        
-        // const asciiTable = createASCIITable(10, 20, tableArray);
-        // console.log(asciiTable);
-        
-        // // Create an HTML table
-        // function createHTMLTable(arr) {
-            //   const tableRows = arr.map((number, index) => {
-                //     return `<tr><td>${number}</td></tr>`;
-                //   }).join('\n');
-                
-                //   return `<table>${tableRows}</table>`;
-                // }
-                
-                // const htmlTable = createHTMLTable(numbersArray);
-                
-                // // Write the ASCII table to a text file
-                // fs.writeFileSync('asciiTable.txt', asciiTable, 'utf-8');
-                
-                // // Write the HTML table to an HTML file
-                // const htmlContent = `<!DOCTYPE html>
-                // <html>
-                // <head>
+//     let rowStr = '';
+//     for (let i = 0; i < col; i++) {
+//       rowStr += `${arr[(row - 1) * col + i]}\t`;
+//     }
+//     return createASCIITable(row - 1, col, arr) + rowStr + '\n';
+//   }
+//   return '';
+// }
+
+// const asciiTable = createASCIITable(10, 20, tableArray);
+// console.log(asciiTable);
+
+// // Create an HTML table
+// function createHTMLTable(arr) {
+//   const tableRows = arr.map((number, index) => {
+//     return `<tr><td>${number}</td></tr>`;
+//   }).join('\n');
+
+//   return `<table>${tableRows}</table>`;
+// }
+
+// const htmlTable = createHTMLTable(numbersArray);
+
+// // Write the ASCII table to a text file
+// fs.writeFileSync('asciiTable.txt', asciiTable, 'utf-8');
+
+// // Write the HTML table to an HTML file
+// const htmlContent = `<!DOCTYPE html>
+// <html>
+// <head>
 //   <title>HTML Table</title>
 // </head>
 // <body>
@@ -88,7 +90,6 @@ result = result.reverse();
 // fs.writeFileSync('htmlTable.html', htmlContent, 'utf-8');
 
 // console.log('Files created: asciiTable.txt and htmlTable.html');
-
 
 // write file
 // fs.writeFileSync("test.txt","hello")
