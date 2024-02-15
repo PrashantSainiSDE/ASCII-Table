e6a4f10f9406df0ce3f9b8162cf0762c74e9dac2
Prince34998 <112163728+Prince34998@users.noreply.github.com>
2024-02-15 11:50:16 +0530

    feat(html) : Add html file format

--- table.js
+++ table.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 const tableNumber = 20;
 
+// with key value
 const tableArray = [
     ..."x"
         .repeat(tableNumber + 1)
@@ -25,23 +26,59 @@ const newTbl = tblArray.reverse().map((value, index, array) => {
     return array.filter((a) => a <= 10);
 });
 
-const tableArr = newTbl.map((val, idx) => val.map((value) => value * (idx + 1)));
+const tableArr = newTbl.map((val, idx) =>
+    val.map((value) => value * (idx + 1))
+);
 
-var asciiTbl = '';
-tableArr.map((val) => { 
-    let test = ''
+// text file string
+var asciiTbl = "";
+tableArr.map((val) => {
+    let test = "";
     val.map((value) => {
-        test += `| ${value.toString().length == 3 ? value:value.toString().length == 2 ? value+" " : value+"  "} |\t`
-    })
-     asciiTbl += `${test}\n`
-})
-
-console.log(asciiTbl);
+        test += `| ${
+            value.toString().length == 3
+                ? value
+                : value.toString().length == 2
+                ? value + " "
+                : value + "  "
+        } |\t`;
+    });
+    asciiTbl += `${test}\n`;
+});
 
 try {
     // Write to a text file
-    fs.writeFileSync('asciiTable.txt', asciiTbl);
-    
+    fs.writeFileSync("asciiTable.txt", asciiTbl);
 } catch (error) {
-  console.log(error);  
+    console.log(error);
+}
+
+// insert data in table
+var htmlTbl = "";
+tableArr.map((val) => {
+    let tblRow = "<tr>\n";
+    val.map((value) => {
+        tblRow += `<td>${value}</td>\n`;
+    });
+    tblRow += "</tr>\n";
+    htmlTbl += tblRow;
+});
+
+// html format string
+const htmlFormat = `<html>
+<head>
+<title>ASCII Table</title>
+</head>
+<body>
+<table border=1 cellpadding="10"> 
+${htmlTbl}
+</table>
+</body>
+</html>`;
+
+try {
+    // Write to a html file
+    fs.writeFileSync("tableHTML.html", htmlFormat);
+} catch (error) {
+    console.log(error);
 }
